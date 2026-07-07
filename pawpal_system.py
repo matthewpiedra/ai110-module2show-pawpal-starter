1e52b52f81db667a181c745e9afcf1c5664146aa
matthewpiedra <matthewluispiedra@gmail.com>
2026-07-06 20:56:04 -0400

    chore: final revisions to initial draft design

--- pawpal_system.py
+++ pawpal_system.py
@@ -6,8 +6,9 @@ class Task:
     name: str
     duration: int
     priority: str
+    completion_status: bool
 
-    def edit(self, name=None, duration=None, priority=None):
+    def edit(self, name=None, duration=None, priority=None, completion_status=None):
         pass
 
 
@@ -15,33 +16,42 @@ class Task:
 class Pet:
     name: str
     species: str
+    tasks: list[Task] = field(default_factory=list)
 
     def add_name(self, name):
         pass
 
     def add_species(self, species):
         pass
+    
+    def add_task(self, task):
+        pass
+    
+    def get_task(self, name):
+        pass
+
+    def edit_task(self, task, name=None, duration=None, priority=None, completion_status=None):
+        pass
+    
+    def remove_task(self, name):
+        pass
+    
+    def get_tasks(self):
+        pass
 
 
 @dataclass
 class Owner:
     name: str
-    tasks: list[Task] = field(default_factory=list)
     pets: list[Pet] = field(default_factory=list)
 
     def add_name(self, name):
         pass
 
-    def add_task(self, task):
-        pass
-
-    def edit_task(self, task, name=None, duration=None, priority=None):
+    def add_pet(self, pet):
         pass
     
-    def remove_task(self, task):
-        pass
-
-    def add_pet(self, pet):
+    def get_pets_tasks(self, name):
         pass
 
 
@@ -49,14 +59,14 @@ class Scheduler:
     def generate_schedule(self, owner):
         pass
 
-    def sort_by_priority_and_duration(self, tasks):
+    def sort_by_priority_and_duration(self, owner):
         pass
 
-    def filter_by_time_constraints(self, tasks):
+    def filter_by_time_constraints(self, owner):
         pass
 
-    def handle_conflicts(self, tasks):
+    def handle_conflicts(self, owner):
         pass
 
-    def explain_schedule(self, schedule):
+    def explain_schedule(self, owner):
         pass
